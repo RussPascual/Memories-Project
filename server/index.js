77750d1cc0871a044ba108db4cd785973c7dde43
Russell Pascual <russell.s.pascual@gmail.com>
2021-01-04 13:58:05 -0800

    completed updating, deleting, and liking capabilities. deployed to web

--- server/index.js
+++ server/index.js
@@ -2,22 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
 
-// FIX <password> LATER
-const CONNECTION_URL = 'mongodb+srv://RussellPascual:RazzleDazzle2312@cluster0.6hzn3.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
